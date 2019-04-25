85cb5f30dce2877199be634ae9a1d8ee16d1885c
shadowmoonqi <32220342+shadowmoonqi@users.noreply.github.com>
2019-04-26 06:25:36 +0800

    add lastUpdated.bat
    用于清理maven仓库下载异常的产生的.lastUpdated文件

---
+++ lastUpdated.bat
@@ -0,0 +1,8 @@
+rem create by sunyan(ysun9621@gmail.com)
+set REPOSITORY_PATH=D:\develop\maven_repository
+rem ÕýÔÚËÑË÷...
+for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
+    del /s /q %%i
+)
+rem ËÑË÷Íê±Ï
+pause 

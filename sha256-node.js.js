6379e7925b6d76541189be3bd73e1ba719a77a73
cloudshadow7 <32220342+cloudshadow7@users.noreply.github.com>
2020-04-12 10:51:44 +0800

    删除无用js，备份插件

--- sha256-node.js.js
+++
@@ -1,5 +0,0 @@
-var crypto = require('crypto');
-var hash = crypto.createHash('sha256');// sha256或者md5
-hash.update('123456');//'123456'为要加密的字符串
-var res = hash.digest('hex');
-console.log(res);// 8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
